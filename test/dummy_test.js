2fe45f05944864076d09ec4f27eb882bf3df96de
James Coglan <james@neighbourhood.ie>
2025-04-28 16:41:04 +0100

    Add a slow test

--- test/dummy_test.js
+++ test/dummy_test.js
@@ -10,4 +10,12 @@ describe('project', () => {
 
 describe('main', () => {
   it('is ok', () => {})
+
+  it('runs a slow test', async () => {
+    await sleep (5 * 60)
+  })
 })
+
+function sleep (sec) {
+  return new Promise((resolve) => setTimeout(resolve, sec * 1000))
+}
